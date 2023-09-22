682374de31767a50d91ff1a9268bc5d6f17a418e
Victor <55928886+vpvpvpvp@users.noreply.github.com>
2023-09-22 18:12:27 +0300

    Integrate segmentation to the app

--- frontend/app.py
+++ frontend/app.py
@@ -7,6 +7,35 @@ import numpy as np
 import cv2
 from PIL import Image
 
+from models_inference.segmentation_model import preprocess_frame, SegmentationModelWrapper
+
+
+def plot_railway_masks(image: np.ndarray, mask: np.ndarray) -> np.ndarray:
+    class_colors = np.array(
+        [
+            [200, 50, 20],
+            [10, 200, 20],
+            [15, 200, 200]
+        ]
+    ).astype(np.float16)
+
+    viz_img = image.copy()
+
+    for i in range(3):
+        cls_num = i + 1
+
+        viz_img[mask == cls_num] = (
+                image[mask == cls_num].astype(np.float16) * 0.4 +
+                class_colors[i] * 0.6
+        ).astype(np.uint8)
+
+    return viz_img
+
+
+
+def get_segmentation_model():
+    return SegmentationModelWrapper("./data/seg.onnx")
+
 
 if __name__ == "__main__":
     st.set_page_config(
@@ -50,6 +79,8 @@ if __name__ == "__main__":
         """,
         unsafe_allow_html=True)
 
+    sm = get_segmentation_model()
+    
     with st.form("my_form"):
         col1, col2 = st.columns(2)
 
@@ -70,14 +101,15 @@ if __name__ == "__main__":
         
         st.warning("Внимание! Обработка занимает некоторое время. Пожалуйста, не перезагружайте страницу!", icon="🚨")
 
-        progress_text = "Нейронки за работой.."
-        my_bar = st.progress(0, text=progress_text)
+        # progress_text = "Нейронки за работой.."
+        # my_bar = st.progress(0, text=progress_text)
 
         with open(uploaded_file.name, mode='wb') as f:
             f.write(uploaded_file.read())
 
         stream = cv2.VideoCapture(uploaded_file.name, cv2.CAP_FFMPEG)
 
+        # stream_length = int(stream.get(cv2.CAP_PROP_FRAME_COUNT))
         frame_display = st.empty()
         stop_button = st.button(label="Остановить")
 
@@ -88,8 +120,11 @@ if __name__ == "__main__":
             if not grabbed:
                 break
             
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            frame_display.image(Image.fromarray(frame), channels="BGR")
-            time.sleep(0.01)
+            # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            mask = sm(frame)
+            vis_frame = plot_railway_masks(frame, mask)
+        
+            frame_display.image(Image.fromarray(vis_frame), channels="BGR")
+            # time.sleep(0.01)
             if stop_button:
                 grabbed = False
